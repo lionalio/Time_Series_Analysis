cd1b761c63b6d02069a2e1e79f9e685c8f4ad90a
Lion Alio <lionalio87@gmail.com>
2021-08-03 19:16:44 -0700

    can work with multiple columns for X data

--- common/TSA.py
+++ common/TSA.py
@@ -10,7 +10,7 @@ class TimeSeriesAnalysis(DataPreparation):
 
     def convert_to_lstm_format(self, data):
         data1 = np.array(data)
-        data1 = np.reshape(data1, (data1.shape[0], data1.shape[1], 1))
+        data1 = np.reshape(data1, (data1.shape[0], data1.shape[1], data1.shape[2]))
         
         return data1
 
@@ -21,29 +21,29 @@ class TimeSeriesAnalysis(DataPreparation):
         
         return out
 
-    def predict_single_instance(self, col, window=60):
+    def predict_single_instance(self, colx, coly, window=60):
         # Training phase
-        X_train = self.get_windowed_data(self.data_train[col], window)
-        y_train = np.array([self.data_train[col][i] for i in range(window, len(self.data_train[col]))])
+        X_train = self.get_windowed_data(self.data_train[colx], window)
+        y_train = np.array([self.data_train[coly][i] for i in range(window, len(self.data_train[coly]))])
         X_train = self.convert_to_lstm_format(X_train)
-        model = model_lstm(input_shape=(X_train.shape[1], 1))
+        model = model_lstm(input_shape=(X_train.shape[1], X_train.shape[2]))
         model.fit(X_train, y_train, epochs=10, batch_size=16, verbose=1)
 
         # Testing phase
         test_data = self.df[len(self.df)- len(self.data_test) - window:]        
-        X_test = self.get_windowed_data(test_data[col], window)
+        X_test = self.get_windowed_data(test_data[colx], window)
         X_test = self.convert_to_lstm_format(X_test)
         preds = model.predict(X_test)
         preds = self.methods['scaler'].inverse_transform(preds)[:, 0]
 
         # Showing results
         train_data = pd.DataFrame()
-        train_data[col] = self.methods['scaler'].inverse_transform(self.data_train[col].values.reshape(-1, 1))[:, 0]
+        train_data[coly] = self.methods['scaler'].inverse_transform(self.data_train[coly].values.reshape(-1, 1))[:, 0]
         train_data.index = self.data_train.index
         valid_data = self.data_test
         valid_data['predictions'] = preds
-        valid_data[col] = self.methods['scaler'].inverse_transform(self.data_test[col].values.reshape(-1, 1))[:, 0]
+        valid_data[coly] = self.methods['scaler'].inverse_transform(self.data_test[coly].values.reshape(-1, 1))[:, 0]
         print(valid_data)
-        plt.plot(train_data[col])
-        plt.plot(valid_data[[col,"predictions"]])
+        plt.plot(train_data[coly])
+        plt.plot(valid_data[[coly,"predictions"]])
         plt.show()
